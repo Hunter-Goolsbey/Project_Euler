9a407cc92ab557912396e9f5451b00d2246f11a1
Hunter-Goolsbey <110963084+Hunter-Goolsbey@users.noreply.github.com>
2022-09-07 18:08:51 -0700

    Create Problem_3.py

---
+++ Problem_3.py
@@ -0,0 +1,14 @@
+# Largest prime factor
+
+
+def P3(number):
+    p=2
+    n=number
+    while n > p**2:
+        if n%p == 0:
+            #print(p)
+            n = n/p
+        else:
+            p = p + 1
+            
+P3(600851475143)
