8ac88a014602935637faf7de1a1e3017feb30c90
Hunter-Goolsbey <110963084+Hunter-Goolsbey@users.noreply.github.com>
2022-10-13 23:36:38 -0700

    Update Problem_6.py

--- Problem_6.py
+++ Problem_6.py
@@ -1,18 +1,19 @@
 #Problem 6
 
-arr1 = []
-arr2 = []
-counter = 0
+def sumsqdif(naturalNum):
+    arr1 = []
+    arr2 = []
+    counter = 0
 
-for i in range(1,101):
-    
-    arr1.append(i**2)
-    arr2.append(i)
-    
-sumsq = sum(arr1)
-sqsum = sum(arr2)**2
+    for i in range(1,naturalNum+1):
+        
+        arr1.append(i**2)
+        arr2.append(i)
+        
+    sumsq = sum(arr1)
+    sqsum = sum(arr2)**2
 
-##print(sumsq)
-##print(sqsum)
+    print("Difference: " + str(sqsum - sumsq))
 
-print("Difference: " + str(sqsum - sumsq))
+
+sumsqdif(100)
